0f1842e5a6c06fe9d08608d29dc1acc5687d079a
Hassanamiri <hassanamiri369@gmail.com>
2022-12-04 00:15:14 +0330

    added Header and Routing and Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React , {useContext , useState} from 'react';
+import {Routes , Route , Link} from "react-router-dom"
+
+import { PostContext } from './context/postContext';
+import Footer from './Layout/Footer/Footer';
+import Header from './Layout/Headr/Header';
+import AddPost from './Pages/AddPost/AddPost';
+import PostsList from './Pages/Posts/PostsList';
+
 
 function App() {
+
+  
+  const context = useContext(PostContext)
+  console.log(context)
+
+  
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  
+      
+      {/* header */}
+      <div className='header'>
+      <Header/>
+      </div>
+
+      <div className='main'>
+        <Routes>
+          <Route path="/" >
+            <Route index element={<PostsList/>}/>
+          </Route>
+
+          <Route path='/addPost' element={<AddPost/>}/>
+        </Routes>
+      </div>
+
+
+      {/* footer */}
+      <div className='footer'>
+      <Footer/>
+      </div>
     </div>
   );
 }

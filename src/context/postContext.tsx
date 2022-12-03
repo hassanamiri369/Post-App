e34f228f06564373eea532c0ed7a9df3de83e7e1
Hassanamiri <hassanamiri369@gmail.com>
2022-12-03 23:52:59 +0330

    create post context file for manage front state

---
+++ src/context/postContext.tsx
@@ -0,0 +1,69 @@
+import React , {createContext , useReducer , useEffect} from "react";
+import {getPost } from "../api/api"
+
+
+interface IPostContextChildren {
+    children : React.ReactNode | JSX.Element
+}
+
+interface IPost {
+    id : number ;
+    title : string;
+    description : string;
+    category : string
+}
+
+interface IState {
+    posts : IPost[]
+}
+
+type IAction = |
+    {type : "addPost" , payload : IPost[] }|
+    {type : "deletePost" , payload : IPost[] }|
+    {type : "editPost" , payload : IPost[]}
+
+
+
+export const PostContext = createContext<any>([])
+
+
+const initState = {
+    posts :[]
+}
+
+const reducer = (state : IState ,  action : IAction)=>{
+    switch(action.type){
+        case "addPost":
+            return{...state , posts : action.payload}
+
+        default : 
+            return state
+    }
+}
+
+const PostContextProvider = ({children} : IPostContextChildren) =>{
+
+
+    const [state , dispatch ]= useReducer(reducer , initState)
+
+
+    // getAll post
+    React.useEffect( ()=>{
+        const getALl = async ()=>{
+            const res : IPost[] = await getPost()
+        if(res){
+            dispatch({type : "addPost" , payload : res})
+        }
+        }
+
+        getALl()
+    } , [])
+
+    return (
+        <PostContext.Provider value={{state , dispatch}}>
+            {children}
+        </PostContext.Provider>
+    )
+}
+
+export default PostContextProvider

db799c8865c2e7e4208bd8bd5835ac393eb28c3a
Hassanamiri <hassanamiri369@gmail.com>
2022-12-05 00:39:32 +0330

    added NavLink and style them

--- src/Layout/Headr/Header.tsx
+++ src/Layout/Headr/Header.tsx
@@ -1,11 +1,36 @@
 import React from 'react'
-import {Link} from "react-router-dom"
+import {NavLink} from "react-router-dom"
+
+// style 
+import "./Style.css"
+
 const Header = () => {
+    // const [menu , setMenu] = React.useState(false)
+
+    // const handleMenu = ()=> setMenu(!menu)
+
+    const NavbarLink = ({isActive } : any) => {
+        return{
+            backgroundColor : isActive ? "tomato" : "white",
+            color : isActive ? "white" : "tomato"
+        }
+    }
+
+
     return (
-        <div>
-            <Link to={'/post'}>PostList</Link>
-            <Link to={'/addPost'}>Add Post</Link>
-            <Link to={'/about'}>About</Link>
+        <div className='container-header'>
+
+
+
+
+
+
+
+           
+            <NavLink style={NavbarLink}  to={'/'}>Home</NavLink>
+            <NavLink style={NavbarLink} to={'/post'}>PostList</NavLink>
+            <NavLink style={NavbarLink} to={'/addPost'}>Add Post</NavLink>
+            <NavLink style={NavbarLink} to={'/about'}>About</NavLink>
         </div>
     )
 }

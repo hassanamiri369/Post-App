f22fd013b51abacd8ea86a8e0affe570b15a4a98
Hassanamiri <hassanamiri369@gmail.com>
2022-12-04 15:40:16 +0330

    show all post and create link for show detail post

--- src/Pages/Posts/PostsList.tsx
+++ src/Pages/Posts/PostsList.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import React , {useContext} from 'react'
+import { PostContext } from '../../context/postContext'
+
+import { Outlet , Link} from 'react-router-dom'
+
+
+// style
+import "./Style.css"
 
 const PostsList = () => {
+
+  const context = useContext(PostContext)
+  const {state} = context;
+  
   return (
-    <div>
+    <div className='post-container'>
         <h1>Post List</h1>
+        <div className='post'>
+          {state.posts.length ===  0 && <h2>no any post !!</h2>}
+          {state.posts.map((item , index) => <div className='content-post' key={item.id}>
+            <div>Post : {index}</div>
+            <div className='title-category'><h2>{item.title}</h2><span>{item.category}</span></div>
+            <p className='description'>{item.description}</p>
+            <p><Link to={`${item.id}`}>more</Link></p>
+          </div>)}
+        </div>
+        <Outlet/>
     </div>
   )
 }
